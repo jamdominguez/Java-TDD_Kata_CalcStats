beded06b9a419e229ba9f8f58553b3ed8f4d57a0
Munoz Dominguez <jomu@gft.com>
2019-07-29 11:05:54 +0200

    minValue, test, develop and refactor

--- src/main/java/CalcStats.java
+++ src/main/java/CalcStats.java
@@ -1,4 +1,16 @@
 public class CalcStats {
-    public CalcStats(int[] nums) {
+
+    private int[] nums;
+
+    public CalcStats(int[] nums) throws Exception {
+        if (nums == null) throw new Exception("Input is null, not is possible calculate stats from this input");
+        if (nums.length == 0) throw new Exception("Input is null, not is possible calculate stats from this input");
+        this.nums = nums;
+    }
+
+    public int minValue() {
+        int min = this.nums[0];
+        for (int i = 1; i < nums.length; i++) if (nums[i] < min) min = nums[i];
+        return min;
     }
 }
--- src/test/java/CalcStatsTest.java
+++ src/test/java/CalcStatsTest.java
@@ -4,12 +4,71 @@ import org.junit.Test;
 public class CalcStatsTest {
 
     @Test
-    public void calcStatsConstructWithIntArray() {
+    public void calcStatsConstructWithIntArray() throws Exception {
         int[] nums = {0, 1, 2, 3};
         CalcStats calcStats = new CalcStats(nums);
-        Assert.assertEquals(true, calcStats instanceof  CalcStats);
+        Assert.assertEquals(true, calcStats instanceof CalcStats);
 
         calcStats = new CalcStats(new int[]{4, 5, 6});
-        Assert.assertEquals(true, calcStats instanceof  CalcStats);
+        Assert.assertEquals(true, calcStats instanceof CalcStats);
     }
+
+    @Test(expected = Exception.class)
+    public void nullInputProducesException() throws Exception {
+        CalcStats calcStats = new CalcStats(null);
+    }
+
+    @Test(expected = Exception.class)
+    public void emptyArrayInputProducesException() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{});
+    }
+
+    @Test
+    public void minValueMustBe0WithInput0() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{0});
+        Assert.assertEquals(0, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe0WithInput0_1() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{0, 1});
+        Assert.assertEquals(0, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe1WithInput1_2() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{1, 2});
+        Assert.assertEquals(1, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe1WithInput1_2_3_4() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{1, 2, 3, 4});
+        Assert.assertEquals(1, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe1WithInput4_3_2_1() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{4, 3, 2, 1});
+        Assert.assertEquals(1, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe1WithInput4_1_3_2_5() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{4, 1, 3, 2, 5});
+        Assert.assertEquals(1, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe1negativeWithInput4_1negative_3_2_5() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{4, -1, 3, 2, 5});
+        Assert.assertEquals(-1, calcStats.minValue());
+    }
+
+    @Test
+    public void minValueMustBe11negativeWithInputNegatives() throws Exception {
+        CalcStats calcStats = new CalcStats(new int[]{-4, -1, -3, -11, -5});
+        Assert.assertEquals(-11, calcStats.minValue());
+    }
+
 }
